94fcd17e7abe419d9da041eb5d635ad8d4c760ab
Kaplaugher <kaplaugher94@gmail.com>
2017-08-16 12:11:17 -0400

    components added

--- client/main.jsx
+++ client/main.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Meteor } from 'meteor/meteor';
-import { Tracker } from 'meteor/tracker';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 
-import { Players } from '../imports/api/players';
+import {Players} from '../imports/api/players';
+import TitleBar from './../imports/ui/TitleBar';
+import AddPlayer from './../imports/ui/AddPlayer';
 
 const renderPlayers = (playersList) => {
   return playersList.map((player) => {
-    return <p key={player._id}>{player.name} has {player.score} points</p>;
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} points.
+        <button onClick={() => Players.update(player._id, {$inc: {score: 1}})}>
+          +1
+        </button>
+        <button onClick={() => Players.update(player._id, {$inc: {score: -1}})}>
+          -1
+        </button>
+        <button onClick={() => Players.remove(player._id)}>X</button>
+      </p>
+    );
   });
 };
 
@@ -19,26 +32,24 @@ const handleSubmit = (event) => {
     event.target.playerName.value = '';
     Players.insert({
       name: playerName,
-      score: 0
+      score: 0,
     });
   }
 
-}
+};
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     const players = Players.find().fetch();
     const title = 'ezScore';
-    const name = 'Kyle';
     const jsx = (
       <div>
-        <h1>{title}</h1>
-        <p>hello, {name} </p>
-        <p>This is my second p</p>
+        <TitleBar title={title}/>
         {renderPlayers(players)}
+        <AddPlayer />
         <form onSubmit={handleSubmit}>
-          <input type="text" name="playerName" placeholder="Player Name" />
-          <button>Add Player</button>
+        <input type="text" name="playerName" placeholder="Player Name"/>
+        <button>Add Player</button>
         </form>
       </div>
     );
