d8483393495f8440fea2ddf0feed413a756a2c86
Kaplaugher <kaplaugher94@gmail.com>
2017-08-17 12:58:00 -0400

    added scss

--- imports/ui/Player.jsx
+++ imports/ui/Player.jsx
@@ -5,16 +5,16 @@ export default class Player extends React.Component {
   render() {
     return (
       <div>
-        <p key={this.props.player._id}>
-          {this.props.player.name} has {this.props.player.score} points.
-          <button onClick={() => Players.update(this.props.player._id, {$inc: {score: 1}})}>
+        <div key={this.props.player._id} className="item">
+          <p>{this.props.player.name} has {this.props.player.score} points.</p>
+          <button className="button button--round" onClick={() => Players.update(this.props.player._id, {$inc: {score: 1}})}>
             +1
           </button>
-          <button onClick={() => Players.update(this.props.player._id, {$inc: {score: -1}})}>
+          <button className="button button--round" onClick={() => Players.update(this.props.player._id, {$inc: {score: -1}})}>
             -1
           </button>
-          <button onClick={() => Players.remove(this.props.player._id)}>X</button>
-        </p>
+          <button className="button button--round" onClick={() => Players.remove(this.props.player._id)}>X</button>
+        </div>
       </div>
     );
   }
--- imports/ui/PlayerList.jsx
+++ imports/ui/PlayerList.jsx
@@ -4,7 +4,11 @@ import  Player from './Player';
 export default class PlayerList extends React.Component {
   renderPlayers() {
     if (this.props.players.length === 0) {
-      return <h3>Add a player to get started!</h3>;
+      return (
+        <div className="item">
+          <h3 className="item__message">Add a player to get started!</h3>
+        </div>
+      )
     } else {
       return this.props.players.map((player) => {
         return <Player key={player._id} player={player}/>
